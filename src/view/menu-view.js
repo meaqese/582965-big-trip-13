37cb4a861438574a5261fbfb7b942b94ad6d7ed8
meaqese <meaqese@protonmail.com>
2021-01-03 18:56:24 +0300

    m1-t3

---
+++ src/view/menu-view.js
@@ -0,0 +1,10 @@
+export const menuView = () => {
+  const getTemplate = () => {
+    return `<nav class="trip-controls__trip-tabs  trip-tabs">
+              <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+              <a class="trip-tabs__btn" href="#">Stats</a>
+            </nav>`;
+  };
+
+  return {getTemplate};
+};
